031a8dd08ce429f18061e811cc68e7d4f5001d6d
南山竹 <junjiemars@gmail.com>
2016-03-31 14:32:37 +0800

    cli parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+extern crate getopts;
+
+use getopts::Options;
+use std::env;
+
+fn usage(bin: &str, opts: Options) {
+	let brief = format!("Usage: {} [options...]", bin);
+	print!("{}", opts.usage(&brief));
+}
+
+fn version() {
+	//show version and build info
+}
+
+fn crawl(/*opts: Options*/) {
+	println!("ready to crawl");
+}
+
 fn main() {
-    println!("Hello, world!");
+	let argv: Vec<String> = env::args().collect(); 
+	let bin = argv[0].clone();
+	let mut opts = Options::new();
+
+	opts.optflag("h", "help", "this help");
+	opts.optflag("V", "version", "show version and exit");
+	opts.optopt("c", "conf", "set configuration file, default fnail.conf", "<file>");
+
+	let m = match opts.parse(&argv[1..]) {
+		Ok(a) => { a }
+		Err(e) => { panic!(e.to_string()) }
+	};
+
+	if m.opt_present("h") {
+		usage(&bin, opts);
+		return;
+	}
+
+	if m.opt_present("V") {
+		version();
+		return;
+	}
+
+	if m.opt_present("c") {
+		crawl(/*opts*/);
+		return;
+	}
+
+	usage(&bin, opts)
 }
